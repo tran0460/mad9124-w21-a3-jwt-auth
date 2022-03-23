af480663a5db0357212e41605d90b1da42794a92
tran0460 <tran0460@algonquinlive.com>
2022-03-23 12:23:49 -0400

    added exception

---
+++ exceptions/ResourceNotFoundException.js
@@ -0,0 +1,12 @@
+class ResourceNotFoundException extends Error {
+    constructor(...arg) {
+        super(...arg)
+        Error.captureStackTrace(this, ResourceNotFoundException)
+        this.code = 404 // this will trigger correct handling by our middleware
+        this.status = '404'
+        this.title = 'Resource does not exist'
+        this.description = this.message
+    }
+}
+
+new ResourceNotFoundException('some error message')
